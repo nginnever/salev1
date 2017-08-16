ebd4e583fed57ac6a3793218947d712c5008f492
nginnever <ginneversource@gmail.com>
2017-08-16 10:07:04 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 test sale structures
+
+## Truffle
+
+```node node_modules/truffle/build/cli.bundled.js test```
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var CrowSale = artifacts.require('./CrowdSale.sol')
+
+module.exports = function(deployer) {
+  //deployer.deploy();
+};
