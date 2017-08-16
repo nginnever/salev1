2657884779c048e9b2e0ba98db6219778e484f2c
nginnever <ginneversource@gmail.com>
2017-08-16 10:21:08 -0500

    remove ds_store

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: "0x0040077926585455c40ceA126B37bED392aCa8C2"
     }
   }
 };
